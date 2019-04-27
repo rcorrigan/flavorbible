c0e6a245397e4d79950422a94665c91741dff45e
mrendleman <michael-rendleman@uiowa.edu>
2019-04-27 15:30:23 -0500

    the model is closer to being functional now!

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -10,6 +10,7 @@ class IngredientsController < ApplicationController
   # GET /ingredients/1
   # GET /ingredients/1.json
   def show
+    @neighbors = Ingredient.associations
   end
 
   # GET /ingredients/new
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,8 @@ class Ingredient
   include Neo4j::ActiveNode
 
   id_property :id
-
+  has_many :both, :neighbor20, type: :"20", model_class: [:Ingredient]
+  has_many :both, :neighbor1, type: :"1", model_class: [:Ingredient]
+  has_many :both, :neighbor5, type: :"5", model_class: [:Ingredient]
+  has_many :both, :neighbo60r, type: :"60", model_class: [:Ingredient]
 end
