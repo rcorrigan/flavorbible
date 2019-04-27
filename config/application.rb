57bbb0129b960475bc5475c77c5e299f85e0bd67
mrendleman <michael-rendleman@uiowa.edu>
2019-04-27 13:52:43 -0500

    a couple more things that may be broken

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,9 @@ module Flavorbible
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.generators{ |g| g.orm :neo4j }
-    config.neo4j.session_type = :http
-    config.neo4j.session_path = 'http://localhost:7474'
-    config.neo4j.session_options = { basic_auth: { username: 'neo4j', password: 'military'} }
+    config.neo4j.session.type = :bolt
+    config.neo4j.session.path = 'http://localhost:7474'
+    config.neo4j.session.options = { basic_auth: { username: 'neo4j', password: 'military'} }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
