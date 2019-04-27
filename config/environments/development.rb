0c3e992ae4d466911cb142f76adb52f7f599dea9
mrendleman <michael-rendleman@uiowa.edu>
2019-04-27 14:20:44 -0500

    this may be less borken?

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,5 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.neo4j.session.type = :bolt
 end
