a816d1cc48780efc2327c1a575baf40f6ae63e14
mrendleman <michael-rendleman@uiowa.edu>
2019-04-29 12:54:08 -0500

    updated models and controllers for new preprocessed db

--- app/controllers/flavorgraphs_controller.rb
+++ app/controllers/flavorgraphs_controller.rb
@@ -7,10 +7,7 @@ class FlavorgraphsController < ApplicationController
   def index
     # Default, render index template to display flavor graphs
     query_ingredient = Ingredient.find("chicken")
-    @connected_ingredients1 = query_ingredient.neighbor1
-    @connected_ingredients5 = query_ingredient.neighbor5
-    @connected_ingredients20 = query_ingredient.neighbor20
-    @connected_ingredients60 = query_ingredient.neighbor60
+    @connected_ingredients = query_ingredient.neighbor
 
   end
 
@@ -30,10 +27,7 @@ class FlavorgraphsController < ApplicationController
   def show
     # Render show template - may need to move index actions here to help with id issue
     @query_ingredient = Ingredient.find(params[:id])
-    @connected_ingredients1 = @query_ingredient.neighbor1
-    @connected_ingredients5 = @query_ingredient.neighbor5
-    @connected_ingredients20 = @query_ingredient.neighbor20
-    @connected_ingredients60 = @query_ingredient.neighbor60
+    @connected_ingredients = @query_ingredient.neighbor
   end
 
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,8 +2,5 @@ class Ingredient
   include Neo4j::ActiveNode
 
   id_property :id
-  has_many :both, :neighbor20, type: :"20", model_class: [:Ingredient]
-  has_many :both, :neighbor1, type: :"1", model_class: [:Ingredient]
-  has_many :both, :neighbor5, type: :"5", model_class: [:Ingredient]
-  has_many :both, :neighbor60, type: :"60", model_class: [:Ingredient]
+  has_many :both, :neighbor, type: :"RELATED", model_class: [:Ingredient]
 end
