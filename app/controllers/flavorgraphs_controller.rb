2145a1d7fbc536d737bd1a8e0d0820e07e26e0d9
rcorrigan <rae-corrigan@uiowa.edu>
2019-04-29 11:33:48 -0500

    Added show pages for individual ids

--- app/controllers/flavorgraphs_controller.rb
+++ app/controllers/flavorgraphs_controller.rb
@@ -1,11 +1,12 @@
 class FlavorgraphsController < ApplicationController
   def new
     # Default, render new template
+    #@input_ingredient = Ingredient.new(:flavor)
   end
 
   def index
     # Default, render index template to display flavor graphs
-    query_ingredient = Ingredient.find("monkfish")
+    query_ingredient = Ingredient.find("chicken")
     @connected_ingredients1 = query_ingredient.neighbor1
     @connected_ingredients5 = query_ingredient.neighbor5
     @connected_ingredients20 = query_ingredient.neighbor20
@@ -14,6 +15,17 @@ class FlavorgraphsController < ApplicationController
   end
 
   def create
-    redirect_to flavorgraphs_path
+    ingredient = Ingredient.find(params[:id])
+    redirect_to flavorgraph_path(ingredient.id)
   end
+
+  def show
+    # Render show template - may need to move index actions here to help with id issue
+    query_ingredient = Ingredient.find(params[:id])
+    @connected_ingredients1 = query_ingredient.neighbor1
+    @connected_ingredients5 = query_ingredient.neighbor5
+    @connected_ingredients20 = query_ingredient.neighbor20
+    @connected_ingredients60 = query_ingredient.neighbor60
+  end
+
 end
