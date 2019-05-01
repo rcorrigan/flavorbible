ec538f62193966ac68bd47cbfd20aa0d80d05296
mrendleman <michael-rendleman@uiowa.edu>
2019-05-01 16:24:19 -0500

    PUSH QUICK BEFORE IT STOPS WORKING

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,7 @@ class Ingredient
   include Neo4j::ActiveNode
 
   id_property :id
+  property :count
   has_many :both, :relations, rel_class: :RelatedTo
 
 end
--- db/neo4j/migrate/20190429182653_create_related_to.rb
+++
@@ -1,9 +0,0 @@
-class CreateRelatedTo < Neo4j::Migrations::Base
-  def up
-    add_constraint :RelatedTo, :uuid
-  end
-
-  def down
-    drop_constraint :RelatedTo, :uuid
-  end
-end
